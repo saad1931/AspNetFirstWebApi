1b44795cc05d10a35568bc4ace83f13c5522de02
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-12-26 02:43:58 +0500

    Add project files.

---
+++ AspNetFirstWebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetFirstWebApi.Controllers
+{
+    public class EmployeesDataController : ApiController//specifies that it is a API controller
+    {
+        public string[] EmployyesArray = { "Saad","Haris","Zubair","Arshad","Azam" };
+
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return EmployyesArray;
+        }
+        [HttpGet]
+        public string GetEmployeesByIndex(int ID)
+        {
+            return EmployyesArray[ID];
+        }
+    }
+}
